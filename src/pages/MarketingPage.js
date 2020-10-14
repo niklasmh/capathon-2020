f611ef122a6d537ab9b64fbe7b1fe1c74e790a87
Hanne K. Ødegård <hanne.1998@live.no>
2020-10-14 20:27:50 +0200

    add market tab

--- src/pages/MarketingPage.js
+++ src/pages/MarketingPage.js
@@ -1,11 +1,11 @@
-//import React from 'react';
+import React from 'react';
 import '../styles/styles.css';
 import Poster from "../pics/poster.png";
 
 function MarketingPage(props) {
   return (
     <div className="poster-container">
-    <img src={Poster}></img>
+      <img src={Poster}></img>
     </div>
   )
 }
