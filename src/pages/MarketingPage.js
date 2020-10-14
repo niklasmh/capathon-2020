1948e90dcf085f820a2c782c73735b283cbc5653
Hanne K. Ødegård <hanne.1998@live.no>
2020-10-14 20:25:27 +0200

    add bad flyer

--- src/pages/MarketingPage.js
+++ src/pages/MarketingPage.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import '../styles/styles.css';
+import Poster from "../pics/poster.png";
 
 function MarketingPage(props) {
-  return null;
+  return (
+    <div className="poster-container">
+    <img src={Poster}></img>
+    </div>
+  )
 }
 
 export default MarketingPage;
